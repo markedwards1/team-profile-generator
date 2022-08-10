acec520d5986cfdcfc03fe40254735bd77a20138
Mark Edwards <markedwards@Marks-MBP.lan>
2022-08-10 13:30:02 +0800

    basic questions

--- app.js
+++ app.js
@@ -4,12 +4,69 @@ const inquirer = require('inquirer');
 
 
 //plan
-
+function start(){
 //prompt questions
-//Role - list
-//base questions
-//name
-//id
-//email
-//role specific question
-//"WHEN" user answers manager, engineer, or intern there should be an alternative question: Manager - office number, engineer - github, intern - school
+inquirer.prompt([
+    //Start, Managers details
+    {
+        type: 'input',
+        message: "Managers name?",
+        name: 'managers_name'
+    },
+    {
+        type: 'input',
+        message: "Employee id?",
+        name: 'managers_id'
+    },
+    {
+        type: 'input',
+        message: "Manager's email?",
+        name: 'managers_email'
+    },
+    {
+        type: 'input',
+        message: "Manager's office number?",
+        name: 'managers_office_number'
+    },
+    //Role - list
+    {
+        type: 'list',
+        message: "What is the employees role?",
+        choices: ['Engineer', 'Intern'],
+        name: 'employee_role'
+    },
+
+
+
+    //base questions
+    //name
+    //id
+    //email
+    //role specific question
+    //"WHEN" user answers manager, engineer, or intern there should be an alternative question: Engineer - github, intern - school
+
+    {
+        type: 'input',
+        message: "Employee name?",
+        name: 'employee_name'
+    },
+    {
+        type: 'input',
+        message: "Employee id?",
+        name: 'employee_id'
+    },
+    {
+        type: 'input',
+        message: "Employee email?",
+        name: 'employee_email'
+    },
+ 
+
+
+
+
+])
+}
+
+//run application
+start();
