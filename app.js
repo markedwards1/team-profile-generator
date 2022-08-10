e20092e90934e52df13e86f6a5642e5f40f07545
Mark Edwards <markedwards@Marks-MBP.lan>
2022-08-10 13:09:50 +0800

    basic starter plan

---
+++ app.js
@@ -0,0 +1,15 @@
+//Inport inquirer
+const inquirer = require('inquirer');
+
+
+
+//plan
+
+//prompt questions
+//Role - list
+//base questions
+//name
+//id
+//email
+//role specific question
+//"WHEN" user answers manager, engineer, or intern there should be an alternative question: Manager - office number, engineer - github, intern - school
